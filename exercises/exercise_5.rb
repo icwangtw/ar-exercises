1f8a85917fe5e451678d9e3091101969c4500150
Stephen <stephenicwang@gmail.com>
2018-05-22 21:03:01 +0000

    finished assignment

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,9 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+sum = Store.sum(:annual_revenue)
+average = sum / Store.count
+good_performance = Store.where("annual_revenue > ?", 1000000).count
+puts "total sum is #{sum}"
+puts "average revenue is #{average}"
+puts "#{good_performance} store are performing well"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,12 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
